01ffecf9d93deddb950b8f932b5986a8244d878a
ShikiLe <mariajane1897@gmail.com>
2021-03-14 00:24:58 +0300

    feat: add solution to the brackets resolving problem.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (
+                str[i] === bracketsConfig[j][0] &&
+                str[i] === bracketsConfig[j][1]
+            ) {
+                if (str[i] !== stack[stack.length - 1]) {
+                    stack.push(str[i]);
+                } else {
+                    stack.pop();
+                }
+            }
+            if (str[i] === bracketsConfig[j][0]) {
+                stack.push(str[i]);
+            }
+            if (str[i] === bracketsConfig[j][1]) {
+                if (bracketsConfig[j][0] === stack[stack.length - 1]) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return !stack.length;
+};
